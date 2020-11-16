47a363ca8a7ef8495ad5ad047a77eeedae7a48dc
Amaury Bodet <amaury@mailbox.org>
2020-11-16 20:57:26 +0100

    update sudo

--- setup.sh
+++ setup.sh
@@ -81,9 +81,11 @@ fi
 
 
 ### Sudo (part 2)
-# Remove temporary passwordless sudo for wheel group
-sed '/%wheel ALL=(ALL) NOPASSWD: ALL/s/^/# /g' /etc/sudoers | EDITOR='tee' visudo
-# Modify sudoers file to allow members of the wheel group
-sed '/%wheel ALL=(ALL) ALL/s/^# //g' /etc/sudoers | EDITOR='tee' visudo
-# Restore sudo lecture for the default user
-rm /var/db/sudo/lectured/"$default_user"
+if [ "$setup_sudo" != "n" ]; then
+	# Remove temporary passwordless sudo for wheel group
+	sed '/%wheel ALL=(ALL) NOPASSWD: ALL/s/^/# /g' /etc/sudoers | EDITOR='tee' visudo
+	# Modify sudoers file to allow members of the wheel group
+	sed '/%wheel ALL=(ALL) ALL/s/^# //g' /etc/sudoers | EDITOR='tee' visudo
+	# Restore sudo lecture for the default user
+	rm /var/db/sudo/lectured/"$default_user"
+fi
