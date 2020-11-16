b4b6e30c7129b586e76e42fe4cbca4e000e97d5a
Amaury Bodet <amaury@mailbox.org>
2020-11-16 07:15:51 +0100

    update default unprivileged user

--- setup.sh
+++ setup.sh
@@ -15,16 +15,24 @@ fi
 
 
 ### Default unprivileged user
+create_default_user() {
+	useradd -m -G ftp,http,mail,wheel "$1"
+	echo "Set a password for the new user:"
+	passwd "$1"
+}
 users_count=$(ls -x /home | wc -l)
-if [ $users_count -eq 0 ]; then
-	echo "No default unprivileged user found."
-elif [ $users_count -eq 1 ]; then
+if [ "$users_count" -eq 0 ]; then
+	echo "No default unprivileged user found. Creating one..."
+	read -p "Enter new user name: " default_user
+	create_default_user "$default_user"
+elif [ "$users_count" -eq 1 ]; then
 	default_user="$(ls -x /home | tr -d '\n')"
 	echo "Setting default unprivileged user to $default_user"
 else
 	read -p "Enter the name of your default unprivileged user: " default_user
-	while [ -z $default_user ] || [ ! -d /home/$default_user ]; do
+	while [ -z "$default_user" ] || [ ! -d /home/"$default_user" ]; do
 		echo "Either you left the user name empty or the user you entered has no home directory."
 		read -p "Enter the name of your default unprivileged user: " default_user
 	done
+	create_default_user "$default_user"
 fi
